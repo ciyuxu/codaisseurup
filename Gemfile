9e92982672dd26f1e5ce63834df9b05a8fcd063c
ciyuxu <YOUR@EMAIL.com>
2017-09-05 22:11:26 +0200

    Add room view

--- Gemfile
+++ Gemfile
@@ -41,6 +41,11 @@ gem 'jquery-rails', '~> 4.3.1'
 gem 'devise', '~> 4.2'
 
 
+#Use datetimpicker for date and time input
+gem 'momentjs-rails', '>= 2.9.0'
+gem 'bootstrap3-datetimepicker-rails', '~> 4.14.30'
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ User.destroy_all
 
 xu = User.create(email: "ciyuxu@hotmail.com", password: "1234567")
 
-event = Event.create(name: "Hackathon", description: "aaa", location: "Amsterdam", price: 5.0, capacity: 30, includes_food: false, includes_drinks: true, active: true)
+event = Event.create(name: "Hackathon", description: "AAA", location: "Amsterdam", capacity: 30, includes_food: false, includes_drinks: true, starts_at: "05/09/2017 10:00", ends_at: "05/09/2017 18:00", active: true, user: xu)
